63ab98551fb8a6bcb88114c329f0b4b10fba7fc3
SanyaOdare <sanyaodare@gmail.com>
2022-08-08 12:28:47 +0100

    add featured component

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,14 @@
+import "./featured.scss"
+
+const Featured = () => {
+  return (
+    <div className="featured">
+    <div className="top">
+      <h1 className="title">Total Revenue</h1>
+    </div>
+    <div className="bottom"></div>
+    </div>
+  )
+}
+
+export default Featured;
