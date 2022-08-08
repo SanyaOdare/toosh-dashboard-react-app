9b5069650cffc8fb7ff3d67e7dcbea58acc51a04
SanyaOdare <sanyaodare@gmail.com>
2022-08-08 13:34:36 +0100

    install react-circular-progressbar

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,12 +1,19 @@
 import "./featured.scss"
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { CircularProgressbar } from "react-circular-progressbar";
 
 const Featured = () => {
   return (
     <div className="featured">
-    <div className="top">
-      <h1 className="title">Total Revenue</h1>
-    </div>
-    <div className="bottom"></div>
+      <div className="top">
+        <h1 className="title">Total Revenue</h1>
+        <MoreVertIcon fontSize="small" />
+      </div>
+      <div className="bottom">
+        <div className="featuredChart">
+          <CircularProgressbar value={70} text="70%" />
+        </div>
+      </div>
     </div>
   )
 }
