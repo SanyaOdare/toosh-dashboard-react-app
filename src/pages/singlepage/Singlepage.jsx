6b1e29696eddc2b46e18f880e6f45c373d81f082
SanyaOdare <sanyaodare@gmail.com>
2022-08-10 00:28:16 +0100

    define props

--- src/pages/singlepage/Singlepage.jsx
+++ src/pages/singlepage/Singlepage.jsx
@@ -1,6 +1,8 @@
 import "./singlepage.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import Chart from "../../components/chart/Chart";
+import List from "../../components/table/Table";
 
 const Singlepage = () => {
   return (
@@ -8,7 +10,43 @@ const Singlepage = () => {
       <Sidebar />
       <div className="singleContainer">
         <Navbar />
-        single
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img src="https://images.pexels.com/photos/2440526/pexels-photo-2440526.jpeg?auto=compress&cs=tinysrgb&w=1600" 
+              alt="barrel" className="itemImg"                
+              />
+              <div className="details">
+                <h2 className="itemTitle">Jill Roe</h2>
+                <div className="detailItem">
+                  <span className="itemKey">Email:</span>
+                  <span className="itemValue">jroe@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone:</span>
+                  <span className="itemValue">+8 4488 889 876</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">Jill St. 789 Lending Rd. Abuja</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">USA</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right">
+            <Chart aspect={3 / 1} title="User Spending ( Last 6 Months )" />
+          </div>
+        </div>
+        <div className="bottom">
+        <h1 className="title">Last Transactions</h1>
+          <List />
+        </div>
       </div>
     </div>
   );
